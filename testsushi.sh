bbdc9c9730f822b37a81d124ea39e58c3348d5a4
Richard Stanley <2528387+citizenrich@users.noreply.github.com>
2021-10-11 11:49:02 +0200

    update test results for sushi

--- testsushi.sh
+++ testsushi.sh
@@ -10,9 +10,9 @@ else
     echo "time"
 fi
 
-for i in 100 500 1000 1500 2000 2500 2500 3000 3500 4000 4500 5000
+for i in 1 10 50 100 250 500 1000 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000
     do ./she-process.py en $i
-    "${time}" -v sushi -o tests/timed_test_${i}.txt
+    "${time}" -f "Num FSH files: ${i}, Elapsed time: %E, CPU: %P, Max RAM (KB): %M" -a -o tests/time_output_sushi.txt sushi 
     rm -rf fsh-generated/resources/*
     rm -rf input/fsh/en*
 done
